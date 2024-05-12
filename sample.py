eacf2eab1c8a1766e7df889d71d89259ced57d7c
Aamir Habib <109647372+aamir2392@users.noreply.github.com>
2024-05-12 11:53:39 +0500

    Update sample.py

--- sample.py
+++ sample.py
@@ -42,6 +42,6 @@ print("\nHollow Pyramid:")
 print_hollow_pyramid(rows)
 
 print()
-print("<---------- Printing the inverted pyramid again to ensure CICD ---------->")
+print("<---------- Printing the inverted pyramid again to ensure CI-CD ---------->")
 print("\nInverted Pyramid:")
 print_inverted_pyramid(rows)
